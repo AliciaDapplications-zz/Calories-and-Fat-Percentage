7c641848d85b0b597d7be119e285b04262778463
Alicia Dale <boricua21dale@gmail.com>
2016-02-27 11:33:16 -0600

    Initial Commit

---
+++ Calories and Fat Percentage/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Calories and Fat Percentage
+//
+//  Created by Alicia Dale on 2/27/16.
+//  Copyright © 2016 Alicia Dale. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
